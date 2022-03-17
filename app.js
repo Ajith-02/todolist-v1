580e721ad34f94f695c6688eee3534aaf6d32471
pajith551@gmail.com <pajith551@gmail.com>
2022-03-17 22:18:03 +0530

    post v1

--- app.js
+++ app.js
@@ -3,45 +3,34 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var item ="";
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
    
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    // var currentDay = today.getDay();
+    // var day = "";
+    var options = {
+        weekday: "long",
+        month: "long",
+        day: "numeric"
+    };
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-            case 1:
-            day = "Monday";
-            break;
-            case 2:
-            day = "Tuesday";
-            break;
-            case 3:
-            day = "Wednesday";
-            break;
-            case 4:
-            day = "Thursday";
-            break;
-            case 5:
-            day = "Friday";
-            break;
-            case 6:
-            day = "Saturday";
-            break;
-    
-        default:
-            break;
-    }
-   
-    res.render("list", {kindOfDay: day});
+    var day = today.toLocaleDateString("en-US", options);
+
+
+    res.render("list", {kindOfDay: day, newListItem: item});
 });
 
+app.post("/", function(req, res) {
+    var item = req.body.newItem;
+    res.redirect("/") 
+})
 
-app.listen(5000, function(){
-    console.log("server is running in 5000");
+app.listen(3000, function(){
+    console.log("server is running in 3000");
 });
